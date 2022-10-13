f69a49b748e9b5b4d7ab410e7b49afc88d1d965a
mitchgm <mitchgm@bgsu.edu>
2022-10-12 18:23:05 -0700

    added team consts

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const generateHTML = require('./src/generateHTML');
+// this creates the page
+
+
+// Each Teams' const link
+const Employee = require("./lib/Employee")
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+const Manager = require("./lib/Manager.js");
+
+
+
+
+
+
+
+
