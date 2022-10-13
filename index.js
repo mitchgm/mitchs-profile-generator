85dd4f2051c8e2a9c61f7297b56b245d36417ed1
mitchgm <mitchgm@bgsu.edu>
2022-10-12 18:42:37 -0700

    manager prompt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const generateHTML = require('./src/generateHTML');
-// this creates the page
+const inquirer = require("inquirer")
+// this creates the page and links node modules
 
 
 // Each Teams' const link
@@ -9,7 +10,48 @@ const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 const Manager = require("./lib/Manager.js");
 
+const employeeArray = [];
 
+
+function startGenerator() {
+    // function call
+
+    function employeeStart() {
+        // starting the prompt
+        inquirer.prompt(
+            [
+                {
+                    type: "input",
+                    message: "What is the manager's name?",
+                    name: "managerName",
+                },
+                {
+                    type: "input",
+                    message: "What is the manager's id num?",
+                    name: "managerId",
+                },
+                {
+                    type: "input",
+                    message: "What is the manager's email?",
+                    name: "managerEmail",
+                },
+                {
+                    type: "input",
+                    message: "What is the manager's office number?",
+                    name: "managerNumber",
+                },
+
+            ])
+    }
+
+
+
+
+
+
+
+
+};
 
 
 
