e289aedb94235f1c68664228116b6a5dd342a3c2
mitchgm <mitchgm@bgsu.edu>
2022-10-18 19:03:12 -0700

    worked with tutor

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const generateHTML = require('./src/generateHTML');
+const generateMarkdown = require('./src/generateMarkdown');
 const inquirer = require("inquirer")
 // this creates the page and links node modules
 
@@ -13,210 +13,186 @@ const Manager = require("./lib/Manager.js");
 const employeeArray = [];
 
 
-function startGenerator() {
-    // function call
+// function startGenerator() {
 
-    function managerStart() {
-        // starting the prompt
-        inquirer.prompt(
-            [
-                {
-                    // setting the promps, each with a message and going to a variable
-                    type: "input",
-                    message: "What is the manager's name?",
-                    name: "managerName",
-                },
-                {
-                    type: "input",
-                    message: "What is the manager's id num?",
-                    name: "managerId",
-                },
-                {
-                    type: "input",
-                    message: "What is the manager's email?",
-                    name: "managerEmail",
-                },
-                {
-                    type: "input",
-                    message: "What is the manager's office number?",
-                    name: "managerNumber",
-                },
-
-            ])
-            .then(managerInfo => {
-                // putting all the collected info into a variable that can be pushed to our array
-                const { managerName, managerId, managerEmail, managerNumber } = managerInfo;
-                const manager = new Manager(managerName, managerId, managerEmail, managerNumber);
-
-                employeeArray.push(manager)
-                // pushing the info
-            })
-    };
-
-    // repeat for all the employee types
-
-    function internStart() {
-        // starting the prompt
-        inquirer.prompt(
-            [
-                {
-                    // setting the promps, each with a message and going to a variable
-                    type: "input",
-                    message: "What is the intern's name?",
-                    name: "internName",
-                },
-                {
-                    type: "input",
-                    message: "What is the intern's id num?",
-                    name: "internId",
-                },
-                {
-                    type: "input",
-                    message: "What is the intern's email?",
-                    name: "internEmail",
-                },
-                {
-                    type: "input",
-                    message: "What is the intern's office number?",
-                    name: "internNumber",
-                },
-
-            ])
-            .then(internInfo => {
-                // putting all the collected info into a variable that can be pushed to our array
-                const { internName, internId, internEmail, internNumber } = internInfo;
-                const intern = new intern(internName, internId, internEmail, internNumber);
-
-                employeeArray.push(intern)
-                // pushing the info
-            })
-    };
-
-    function engineerStart() {
-        // starting the prompt
-        inquirer.prompt(
-            [
-                {
-                    // setting the promps, each with a message and going to a variable
-                    type: "input",
-                    message: "What is the engineer's name?",
-                    name: "engineerName",
-                },
-                {
-                    type: "input",
-                    message: "What is the engineer's id num?",
-                    name: "engineerId",
-                },
-                {
-                    type: "input",
-                    message: "What is the engineer's email?",
-                    name: "engineerEmail",
-                },
-                {
-                    type: "input",
-                    message: "What is the engineer's office number?",
-                    name: "engineerNumber",
-                },
-
-            ])
-            .then(engineerInfo => {
-                // putting all the collected info into a variable that can be pushed to our array
-                const { engineerName, engineerId, engineerEmail, engineerNumber } = engineerInfo;
-                const engineer = new engineer(engineerName, engineerId, engineerEmail, engineerNumber);
-
-                employeeArray.push(engineer)
-                // pushing the info
-            })
-    };
-
-    // function employeeStart() {
-    //     // starting the prompt
-    //     inquirer.prompt(
-    //         [
-    //             {
-    //                 // setting the promps, each with a message and going to a variable
-    //                 type: "input",
-    //                 message: "What is the employee's name?",
-    //                 name: "employeeName",
-    //             },
-    //             {
-    //                 type: "input",
-    //                 message: "What is the employee's id num?",
-    //                 name: "employeeId",
-    //             },
-    //             {
-    //                 type: "input",
-    //                 message: "What is the employee's email?",
-    //                 name: "employeeEmail",
-    //             },
-    //             {
-    //                 type: "input",
-    //                 message: "What is the employee's office number?",
-    //                 name: "employeeNumber",
-    //             },
-
-    //         ])
-    //         .then(employeeInfo => {
-    //             // putting all the collected info into a variable that can be pushed to our array
-    //             const { employeeName, employeeId, employeeEmail, employeeNumber } = employeeInfo;
-    //             const employee = new employee(employeeName, employeeId, employeeEmail, employeeNumber);
-
-    //             employeeArray.push(employee)
-    //             // pushing the info
-    //         })
-    // };
-
-    function startTeam() {
-        // this is the initial prompt
-        inquirer.prompt([{
-            type: "list",
-            message: "What type of employee will you be collecting info for?",
-            name: "addMember",
-            choices: ["Employee", "Engineer", "Intern", "Manager", "None"]
-            // choice decides the use case for the function
-        }]).then(function (input) {
-            switch (input.addMember) {
-                // each function path based on the initial input
-                // case "Employee":
-                //     employeeStart();
-                //     break;
-                case "Engineer":
-                    engineerStart();
-                    break;
-                case "Intern":
-                    internStart();
-                    break;
-                case "Manager":
-                    managerStart();
-                    break;
-
-
-                default:
-                    defaultCase();
-                // the default will run if nothing is selected or the none is chosen 
-            }
-        })
-    }
-
-
-    function defaultCase() {
+function startTeam() {
+    // this is the initial prompt
+    return inquirer.prompt([{
+        type: "list",
+        message: "What type of employee will you be collecting info for?",
+        name: "addMember",
+        choices: ["Engineer", "Intern", "Manager", "None"]
+        // choice decides the use case for the function
+    }]).then(function (input) {
+        if (input.addMember === "Engineer") {
+            engineerStart()
+        }
+        if (input.addMember === "Intern") {
+            internStart()
+        }
+        if (input.addMember === "Manager") {
+            managerStart()
+        }
+        else {
+            defaultCase()
+        }
         // the default will run if nothing is selected or the none is chosen 
-        fs.writeFileSync(output, generate(employeeArray), "UTF-8")
-    };
 
+    })
+}
 
+const managerStart = () => {
+    // starting the prompt
+    return inquirer.prompt(
+        [
+            {
+                // setting the promps, each with a message and going to a variable
+                type: "input",
+                message: "What is the manager's name?",
+                name: "managerName",
+            },
+            {
+                type: "input",
+                message: "What is the manager's id num?",
+                name: "managerId",
+            },
+            {
+                type: "input",
+                message: "What is the manager's email?",
+                name: "managerEmail",
+            },
+            {
+                type: "input",
+                message: "What is the manager's office number?",
+                name: "managerNumber",
+            },
 
+        ])
+        .then(managerInfo => {
+            // putting all the collected info into a variable that can be pushed to our array
+            const { managerName, managerId, managerEmail, managerNumber } = managerInfo;
+            const manager = new Manager(managerName, managerId, managerEmail, managerNumber);
 
+            employeeArray.push(manager)
+            console.log(employeeArray)
+            // pushing the info
+        })
+};
 
+// repeat for all the employee types
 
-    startTeam();
+function internStart() {
+    // starting the prompt
+    inquirer.prompt(
+        [
+            {
+                // setting the promps, each with a message and going to a variable
+                type: "input",
+                message: "What is the intern's name?",
+                name: "internName",
+            },
+            {
+                type: "input",
+                message: "What is the intern's id num?",
+                name: "internId",
+            },
+            {
+                type: "input",
+                message: "What is the intern's email?",
+                name: "internEmail",
+            },
+            {
+                type: "input",
+                message: "What is the intern's office number?",
+                name: "internNumber",
+            },
 
+        ])
+        .then(internInfo => {
+            // putting all the collected info into a variable that can be pushed to our array
+            const { internName, internId, internEmail, internNumber } = internInfo;
+            const intern = new Intern(internName, internId, internEmail, internNumber);
+
+            employeeArray.push(intern)
+            console.log(employeeArray)
+            // pushing the info
+        })
+};
+
+function engineerStart() {
+    // starting the prompt
+    inquirer.prompt(
+        [
+            {
+                // setting the promps, each with a message and going to a variable
+                type: "input",
+                message: "What is the engineer's name?",
+                name: "engineerName",
+            },
+            {
+                type: "input",
+                message: "What is the engineer's id num?",
+                name: "engineerId",
+            },
+            {
+                type: "input",
+                message: "What is the engineer's email?",
+                name: "engineerEmail",
+            },
+            {
+                type: "input",
+                message: "What is the engineer's office number?",
+                name: "engineerNumber",
+            },
+
+        ])
+        .then(engineerInfo => {
+            // putting all the collected info into a variable that can be pushed to our array
+            const { engineerName, engineerId, engineerEmail, engineerNumber } = engineerInfo;
+            const engineer = new Engineer(engineerName, engineerId, engineerEmail, engineerNumber);
+
+            employeeArray.push(engineer)
+            console.log(employeeArray)
+            // pushing the info
+        })
 };
 
 
 
 
-startGenerator();
+function defaultCase(data) {
+    // the default will run if nothing is selected or the none is chosen 
+    fs.writeFile("./dist/index.html", data, err => {
+        console.log(data)
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("Team Profile Successfully Generated!")
+        }
+    })
+};
+
+
+
+
+
+
+// startTeam();
+
+// };
+
+
+
+
+managerStart()
+    .then(employeeArray => {
+        return generateMarkdown(employeeArray)
+    }).then(htmlData => {
+        return defaultCase(htmlData)
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
 
 
---
+++ src/generateMarkdown.js
@@ -0,0 +1,121 @@
+
+// Template literal for manager section
+const managersCard = function (manager) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${manager.managerName}</h3>
+                <h4>Manager</h4><i class="material-icons">groups</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${manager.managerId}</p>
+                <p class="email">Email: <a href="mailto:${manager.managerEmail}">${manager.managerEmail}</a></p>
+                <p class="office">Office Number: ${manager.managerNumber}</p>
+            </div>
+        </div>
+    </div>`
+};
+
+// // Template literal for engineer section
+// const engineersCard = function (engineer) {
+//     return `
+//     <div class="col-4 mt-4">
+//         <div class="card h-100">
+//             <div class="card-header">
+//                 <h3>${engineer.name}</h3>
+//                 <h4>Engineer</h4><i class="material-icons">engineering</i>
+//             </div>
+//             <div class="card-body">
+//                 <p class="id">ID: ${engineer.id}</p>
+//                 <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+//                 <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+//             </div>
+//         </div>
+//     </div>`
+// };
+
+// // Template literal for intern section
+// const internsCard = function (intern) {
+//     return `
+//     <div class="col-4 mt-4">
+//         <div class="card h-100">
+//             <div class="card-header">
+//                 <h3>${intern.name}</h3>
+//                 <h4>Intern</h4><i class="material-icons">school</i>
+//             </div>
+//             <div class="card-body">
+//                 <p class="id">ID: ${intern.id}</p>
+//                 <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+//                 <p class="school">School: ${intern.school}</p>
+//             </div>
+//     </div>
+// </div>`
+// };
+
+const generateMarkdown = (data) => {
+    pageArray = [];
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole();
+
+        // Calling managersCard
+        if (role === 'Manager') {
+            const managerCard = managersCard(employee);
+            pageArray.push(managerCard);
+        }
+
+        // Calling engineersCard
+        // if (role === 'Engineer') {
+        //     const engineerCard = engineersCard(employee);
+        //     pageArray.push(engineerCard);
+        // }
+
+        // // Calling internsCard 
+        // if (role === 'Intern') {
+        //     const internCard = internsCard(employee);
+        //     pageArray.push(internCard);
+        // }
+
+    }
+    const employeeCards = pageArray.join('')
+    const generateTeam = generateTeamPage(employeeCards);
+    return generateTeam;
+};
+
+// Template literal for HTML boiler plate and links/scripts
+const generateTeamPage = function (employeeCards) {
+    return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Team Profile</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+      <header>
+          <nav class="navbar" id="navbar">
+              <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+          </nav>
+      </header>
+      <main>
+          <div class="container">
+              <div class="row justify-content-center" id="team-cards">
+                  ${employeeCards}
+              </div>
+          </div>
+      </main>
+      
+  </body>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </html>`
+};
+
+module.exports = generateMarkdown;
